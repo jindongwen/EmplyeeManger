7a5f1dc39f7567cc72ded915f0b288cbd8bf4782
jindongwen <717844887@qq.com>
2020-11-12 19:04:46 +0800

    获取表的接口实现完成，继承该接口类的方法声明完成

--- src/com/ylb/dao/EmployeeDao.java
+++ src/com/ylb/dao/EmployeeDao.java
@@ -1,4 +1,44 @@
 package com.ylb.dao;
 
+import com.ylb.entity.Employee;
+
+import java.util.List;
+
+/**
+ * Employee 是员工对象，代表表格的每一行
+ */
+
 public interface EmployeeDao {
+    /**
+     * 列出所有员工
+     * @return
+     */
+    public List<Employee> findAll();
+
+    /**
+     * 查询指定编号员工
+     * @param empno
+     * @return
+     */
+    public Employee findByID(int empno);
+
+    /**
+     * 添加员工
+     * @param emp
+     */
+    public void save(Employee emp);
+
+    /**
+     * 修改员工信息
+     * @param emp
+     * @return
+     */
+    public int update(Employee emp);
+
+    /**
+     * 删除指定编号员工
+     * @param empno
+     * @return
+     */
+    public int delete(int empno);
 }
--- src/com/ylb/dao/impl/EmplyeeDaoImpl.java
+++ src/com/ylb/dao/impl/EmplyeeDaoImpl.java
@@ -1,9 +1,36 @@
 package com.ylb.dao.impl;
 
 import com.ylb.dao.EmployeeDao;
+import com.ylb.entity.Employee;
+
+import java.util.List;
 
 /**
  * 实现接口
  */
 public class EmplyeeDaoImpl implements EmployeeDao {
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Employee findByID(int empno) {
+        return null;
+    }
+
+    @Override
+    public void save(Employee emp) {
+
+    }
+
+    @Override
+    public int update(Employee emp) {
+        return 0;
+    }
+
+    @Override
+    public int delete(int empno) {
+        return 0;
+    }
 }
